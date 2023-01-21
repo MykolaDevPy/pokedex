344d482a7bc44f39a3f403684faaec73ea5c16b9
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-21 14:31:39 +0100

    In 'teams' app modified 'serializers' for correct work and at 'pokemons' app fixed related field in 'PokemonDetailsSerializer'

--- pokedex-back/settings.py
+++ pokedex-back/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = "pokedex-back.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
-}"""
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- teams/signals.py
+++ teams/signals.py
@@ -1,99 +1,51 @@
 from django.db.models.signals import post_save
 from django.db.models.signals import post_delete
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_init
 from django.dispatch import receiver
 
 from .models import Team
 from pokemon.models import Pokemon
 
 
-@receiver(pre_save, sender=Team)
-def verify_ownership(sender, instance, **kwargs):
-    """Verify ownership of a given instance before saving."""
-
-    try:
-        pokemon = Pokemon.objects.get(pk=instance.pokemon_1_id)
-        if pokemon.trainer == instance.trainer:
-            instance.pokemon_1 = pokemon
-        else:
-            instance.pokemon_1 = None
-    except Pokemon.DoesNotExist:
-        instance.pokemon_1 = None
-
-    if instance.pokemon_2_id:
-        try:
-            pokemon = Pokemon.objects.get(pk=instance.pokemon_2_id)
-            if pokemon.trainer == instance.trainer:
-                instance.pokemon_2 = pokemon
-            else:
-                instance.pokemon_2 = None
-        except Pokemon.DoesNotExist:
-            instance.pokemon_2 = None
-
-    if instance.pokemon_3_id:
-        try:
-            pokemon = Pokemon.objects.get(pk=instance.pokemon_3_id)
-            if pokemon.trainer == instance.trainer:
-                instance.pokemon_3 = pokemon
-            else:
-                instance.pokemon_3 = None
-        except Pokemon.DoesNotExist:
-            instance.pokemon_3 = None
-    
-    if instance.pokemon_4_id:
-        try:
-            pokemon = Pokemon.objects.get(pk=instance.pokemon_4_id)
-            if pokemon.trainer == instance.trainer:
-                instance.pokemon_4 = pokemon
-            else:
-                instance.pokemon_4 = None
-        except Pokemon.DoesNotExist:
-            instance.pokemon_4 = None
-    
-    if instance.pokemon_5_id:
-        try:
-            pokemon = Pokemon.objects.get(pk=instance.pokemon_5_id)
-            if pokemon.trainer == instance.trainer:
-                instance.pokemon_5 = pokemon
-            else:
-                instance.pokemon_5 = None
-        except Pokemon.DoesNotExist:
-            instance.pokemon_5 = None
-
-
 @receiver(post_save, sender=Team)
 def assign_to_team(sender, instance, created, **kwargs):
     """Updating the information about team in Pokemon instance."""
+    pokemons = []
 
-    if created:
-        pokemons = []
+    # Getting all pokemons assigned to this team
+    for i in range(1, 6):
+        poke_field = "pokemon_" + str(i)
+        if getattr(instance, poke_field):
+            pokemons.append(Pokemon.objects.get(pk=getattr(instance, poke_field).id))
 
-        for i in range(1,6):
-            poke_field = 'pokemon_' + str(i)
-            if getattr(instance, poke_field):
-                pokemons.append(Pokemon.objects.get(pk=getattr(instance, poke_field).id))
-
-        for pokemon in pokemons:
-            pokemon.team = instance
-            pokemon.save()
-
+    # Executed only if update of DB
     if not created:
-        pokemons = []
         prev_pokemons = Pokemon.objects.filter(team=instance)
 
-        for i in range(1,6):
-            poke_field = 'pokemon_' + str(i)
-            if getattr(instance, poke_field):
-                pokemons.append(Pokemon.objects.get(pk=getattr(instance, poke_field).id))
-        
         for prev_pokemon in prev_pokemons:
             if prev_pokemon not in pokemons:
                 prev_pokemon.team = None
                 prev_pokemon.save()
-
-        for pokemon in pokemons:
-            pokemon.team = instance
-            pokemon.save()
+    
+    # Put the name of current Team in the Pokemon instance
+    for pokemon in pokemons:
+        # Getting previous Teams where could be the same Pokemon
+        another_team = Team.objects.filter(
+            Q(pokemon_1=pokemon) | 
+            Q(pokemon_2=pokemon) | 
+            Q(pokemon_3=pokemon) | 
+            Q(pokemon_4=pokemon) | 
+            Q(pokemon_5=pokemon)
+        ).first()
+        if another_team:
+            for i in range(1, 6):
+                poke_field = "pokemon_%d" % i
+                # Delete current pokemon from previous team
+                if getattr(another_team, poke_field ) == pokemon:
+                    setattr(another_team, poke_field, None)
+                    another_team.save()
+        pokemon.team = instance
+        pokemon.save()
 
 
 @receiver(post_delete, sender=Team)
