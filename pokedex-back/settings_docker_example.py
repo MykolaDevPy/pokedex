e7c0af5d8b22fe0087e84848e159ab4eefbc081e
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-23 16:16:13 +0100

    Setting for connection to PostgreSQl DB

--- pokedex-back/settings_docker_example.py
+++ pokedex-back/settings_docker_example.py
@@ -13,10 +13,10 @@ django_env_overrides.apply_to(globals())
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": POSTGRES_HOST,
-        "NAME": "pokedex",
-        "USER": "postgres",
-        "PASSWORD": "8Fny?aXEFkh9ePA3",
-        "PORT": POSTGRES_PORT,
+        "HOST": PGHOST,
+        "NAME": PGDATABASE,
+        "USER": PGUSER,
+        "PASSWORD": PGPASSWORD,
+        "PORT": PGPORT,
     }
 }
