bde58a40bd7914a5e0ca292dd4d66b147a04aa00
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-23 19:14:54 +0100

    modified entrypoint for import and migrations and changes ENVVAR

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,16 @@
 while !</dev/tcp/$PGHOST/$PGPORT; do echo "En attente du demarrage de postgresql" && sleep 1; done
 if ! PGPASSWORD=$PGPASSWORD psql -U postgres -h $PGHOST -p $PGPORT -lqt | cut -d \| -f 1 | cut -d ' ' -f 2 | grep -q "^railway$"; then
     PGPASSWORD=$PGPASSWORD createdb -U postgres -h $PGHOST -p $PGPORT $PGDATABASE
+    gosu simadm ./manage.py makemigrations
+    gosu simadm ./manage.py migrate
+    gosu simadm ./manage.py import_csv
+    gosu simadm ./manage.py import_objects_csv
 else
-    dropdb -U postgres -h $PGHOST -p $PGPORT -W $PGDATABASE
-    PGPASSWORD=$PGPASSWORD createdb -U postgres -h $PGHOST -p $PGPORT $PGDATABASE
-    echo $PGDATABASE + " created"
+    echo $PGDATABASE + " already exists"
 
 fi
 
 mkdir -p /var/www/static && chown simadm:www-data /var/www/static
 gosu simadm make migrate
 gosu simadm ./manage.py collectstatic --noinput
-gosu simadm ./manage.py makemigrations
-gosu simadm ./manage.py migrate
-gosu simadm ./manage.py import_csv
-gosu simadm ./manage.py import_objects_csv
 exec gosu simadm uwsgi --http-socket :8030 --uid simadm --ini config_files/basic-docker.ini --processes 4 --threads 2 --wsgi-file pokedex-back/wsgi.py
