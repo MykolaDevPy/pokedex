95bae6ea4c03e90e13bdd4e9a7c258351a05f228
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-23 18:48:47 +0100

    Created migrations and modified 'entrypoint' for make migration and import csv to DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,8 @@ fi
 mkdir -p /var/www/static && chown simadm:www-data /var/www/static
 gosu simadm make migrate
 gosu simadm ./manage.py collectstatic --noinput
+gosu simadm ./manage.py makemigrations
+gosu simadm ./manage.py migrate
+gosu simadm ./manage.py import_csv
+gosu simadm ./manage.py import_object_csv
 exec gosu simadm uwsgi --http-socket :8030 --uid simadm --ini config_files/basic-docker.ini --processes 4 --threads 2 --wsgi-file pokedex-back/wsgi.py
