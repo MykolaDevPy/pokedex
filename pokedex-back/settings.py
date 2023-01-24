44c439679a5e7035433d7d734ac2d3baf546574a
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-24 19:44:18 +0100

    changed posibility to si debug

--- pokedex-back/settings.py
+++ pokedex-back/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "False"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/static"
