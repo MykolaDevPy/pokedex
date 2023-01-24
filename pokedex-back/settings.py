cb08f0324d902d2d6a63bdff61a974b72bee5467
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-24 18:56:32 +0100

    changed 'SECRET_KEY' access

--- pokedex-back/settings.py
+++ pokedex-back/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3q@7xs0v_+d5ltnv-0(%$ug!#v1q2(@gqe@leb52k5$#+ya%-%"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
