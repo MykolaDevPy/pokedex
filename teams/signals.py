c34ec70b422fd22ad04d20a1f4221f0d356cab5a
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-25 16:32:35 +0100

    Restructuring of code for 'post_save' method in 'signals' for better readability

--- teams/signals.py
+++ teams/signals.py
@@ -1,8 +1,8 @@
-from django.db.models import Q
 from django.db.models.signals import post_delete
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from .utils import *
 
 from .models import Team
 from pokemon.models import Pokemon
@@ -39,12 +39,7 @@ def assign_to_team(sender, instance, created, **kwargs):
         instance.pokemon_5_id,
     ]
 
-    prev_pokemons = Pokemon.objects.filter(team=instance)
-    if prev_pokemons:
-        for prev_pokemon in prev_pokemons:
-            if prev_pokemon not in pokemons:
-                prev_pokemon.team = None
-                prev_pokemon.save()
+    set_changed_team(pokemons, instance)
 
     # Put the name of current Team in the Pokemon instance
     for pokemon_id in pokemons:
@@ -53,20 +48,7 @@ def assign_to_team(sender, instance, created, **kwargs):
             pokemon.team = instance.name
             pokemon.save(update_fields=["team"])
             # Getting previous Teams where could be the same Pokemon
-            another_team = Team.objects.filter(
-                Q(pokemon_1=pokemon)
-                | Q(pokemon_2=pokemon)
-                | Q(pokemon_3=pokemon)
-                | Q(pokemon_4=pokemon)
-                | Q(pokemon_5=pokemon)
-            ).exclude(pk=instance.id).first()
-            if another_team:
-                for i in range(1, 6):
-                    poke_field = "pokemon_%d" % i
-                    # Delete current pokemon from previous team
-                    if getattr(another_team, poke_field) == pokemon:
-                        setattr(another_team, poke_field, None)
-                        another_team.save(update_fields=[poke_field])
+            check_another_teams(pokemon, instance)
 
 
 @receiver(post_delete, sender=Team)
---
+++ teams/utils.py
@@ -0,0 +1,39 @@
+from django.db.models import Q
+
+from pokemon.models import Pokemon
+from teams.models import Team
+
+
+
+def set_changed_team(pokemons, instance) -> None:
+    """
+    Get all the pokemons of the given team and
+    set None as value for pokemon if he is not more in this team.
+    """
+    
+    prev_pokemons = Pokemon.objects.filter(team=instance)
+    if prev_pokemons:
+        for prev_pokemon in prev_pokemons:
+            if prev_pokemon not in pokemons:
+                prev_pokemon.team = None
+                prev_pokemon.save()
+
+def check_another_teams(pokemon, instance) -> None:
+    """
+    Check if the given pokemon is already in another team and 
+    remove it from previous team if it is.
+    """
+    another_team = Team.objects.filter(
+                Q(pokemon_1=pokemon)
+                | Q(pokemon_2=pokemon)
+                | Q(pokemon_3=pokemon)
+                | Q(pokemon_4=pokemon)
+                | Q(pokemon_5=pokemon)
+            ).exclude(pk=instance.id).first()
+    if another_team:
+        for i in range(1, 6):
+            poke_field = "pokemon_%d" % i
+            # Delete current pokemon from previous team
+            if getattr(another_team, poke_field) == pokemon:
+                setattr(another_team, poke_field, None)
+                another_team.save(update_fields=[poke_field])
