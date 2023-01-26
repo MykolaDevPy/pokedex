ef6c2c2c1bad614bebb87f0fc77d5ec47a750a52
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-26 11:56:31 +0100

    Added test for command 'import_objects_csv'

--- pokedex/tests/test_commands.py
+++ pokedex/tests/test_commands.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 
 from pokemon.models import PokedexCreature
+from favorite_object.models import FavoriteObject
 
 
 def test_wait_for_db_ready() -> None:
@@ -68,3 +69,14 @@ def test_import_invalid_csv_file(capsys) -> None:
         call_command("import_csv", ntf.name)
         expected = "This is not a CSV file.\n"
         assert expected == capsys.readouterr().err
+
+
+@pytest.mark.django_db
+def test_import_favorite_objects_csv_file(capsys) -> None:
+    """Test import favorite objects CSV file"""
+    
+    call_command("import_objects_csv")
+    assert FavoriteObject.objects.count() == 344
+
+    expected = "Nb of favorite objects imported to the database: 344.\n"
+    assert expected == capsys.readouterr().out
