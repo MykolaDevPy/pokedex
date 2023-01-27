dccc9cac81be9fb2c4df800428e054b6a59c2295
Mykola Vradnyk <m.vradnyk@gmail.com>
2023-01-27 17:26:05 +0100

    Fixed order of arguments in 'has_object_permission' method

--- pokemon/permissions.py
+++ pokemon/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import PermissionDenied
 class IsOwner(IsAuthenticated):
     """Check if a user has the given permission"""
 
-    def has_object_permission(self, obj, view, request):
+    def has_object_permission(self, request, view, obj):
         if obj.trainer == request.user or request.user.is_superuser:
             return True
         else:
